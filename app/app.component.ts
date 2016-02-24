565b2485d6d33398d7426a37e3c227aaa90613f9
Florian Witteler <florian_witteler@gmx.de>
2016-02-24 16:22:40 +0100

    tutorial step 5

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,43 @@
-import {Component} from 'angular2/core';
+import {HeroDetailComponent} from "./hero-detail.component";
+import {DashboardComponent} from "./dashboard.component";
+import { Component } from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My Second Angular 2 App</h1>'
+  selector: 'my-app',
+  template: `
+  <h1>{{title}}</h1>
+  <nav>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    <a [routerLink]="['Heroes']">Heroes</a>
+  </nav>
+  <router-outlet></router-outlet>
+`,
+  styleUrls: ['app/app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    ROUTER_PROVIDERS,
+    HeroService
+  ]
 })
-export class AppComponent { }
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  }, {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }, {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  },
+])
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
---
+++ app/heroes.component.ts
@@ -0,0 +1,38 @@
+import {Component} from 'angular2/core';
+import {OnInit} from 'angular2/core';
+import {Router} from "angular2/router";
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
+import {HeroDetailComponent} from './hero-detail.component';
+
+@Component({
+    selector: 'my-heroes',
+    templateUrl: 'app/heroes.component.html',
+    styleUrls: ['app/heroes.component.css'],
+    directives: [HeroDetailComponent],
+    })
+export class HeroesComponent implements OnInit {
+  public heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(
+    private _router: Router,
+    private _heroService: HeroService
+  ) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
+  getHeroes() {
+    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  gotoDetail() {
+    this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+  }
+}
